3acb2e2fd9144473aedcc8eb5760144f29c97069
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-03-11 13:03:59 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPRO_2211_Assignment_3_Trip_Log.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
